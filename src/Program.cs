883a7ef96dbe634b970d7ec6c920e1d48681fed4
ggobbe <guillaume.gobbe@gmail.com>
2017-01-02 13:04:55 +0000

    Prompt the user for username and password

--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,40 @@
-﻿namespace SilverPexer
+﻿using System;
+
+namespace SilverPexer
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Console.Write("Username: ");
+            var username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            var password = ReadPassword();
+
             var pexer = new Pexer();
-            pexer.Login("USERNAME", "PASSWORD");
+            pexer.Login(username, password);
 
             while (true)
             {
                 pexer.KillAllMonsters();
                 pexer.WaitForMonsters();
             }
+        }
+
+        private static string ReadPassword()
+        {
+            string password = null;
+            while (true)
+            {
+                var key = System.Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+                password += key.KeyChar;
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            return password;
         }
     }
 }
