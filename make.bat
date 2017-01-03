5dc95e43217ab17faae2a7c247dac014e898f064
ggobbe <guillaume.gobbe@gmail.com>
2017-01-03 10:32:33 +0100

    Fix build errors due to problem in dotnet build command

--- make.bat
+++ make.bat
@@ -3,6 +3,7 @@ echo [Restore Packages]
 dotnet restore .\src\SilverPexer\
 echo.
 echo [Build]
-dotnet build .\src\SilverPexer\ --output .\bin --configuration Release --framework net452
-echo.
-pause
+rem Building twice as a workaround to https://github.com/dotnet/cli/issues/2871
+dotnet build .\src\SilverPexer\ --configuration Release --framework net452 > .\build.tmp
+dotnet build .\src\SilverPexer\ --configuration Release --framework net452 --output .\bin
+del .\build.tmp
