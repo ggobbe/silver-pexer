32f318344142623aab925a5528cde17b32d95f09
ggobbe <guillaume.gobbe@gmail.com>
2017-04-05 10:02:35 +0200

    Update makefile

--- make.bat
+++ make.bat
@@ -3,7 +3,4 @@ echo [Restore Packages]
 dotnet restore .\src\SilverPexer\
 echo.
 echo [Build]
-rem Building twice as a workaround to https://github.com/dotnet/cli/issues/2871
-dotnet build .\src\SilverPexer\ --configuration Release --framework net462 > .\build.tmp
-dotnet build .\src\SilverPexer\ --configuration Release --framework net462 --output .\bin
-del .\build.tmp
+dotnet build .\src\SilverPexer\ --configuration Release --framework net462 --output ..\..\bin
